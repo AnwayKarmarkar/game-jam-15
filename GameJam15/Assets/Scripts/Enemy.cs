b92fadf02e309e3c4abbf97f8d0045db8237f772
Anway <anway.karmarkar@gmail.com>
2024-07-25 21:18:25 -0400

    merged combat and lighting

--- GameJam15/Assets/Scripts/Enemy.cs
+++ GameJam15/Assets/Scripts/Enemy.cs
@@ -5,165 +5,151 @@ using System.Net.NetworkInformation;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
-public class Enemy : MonoBehaviour
-{
-    [SerializeField]
-    private int health = 100;
-    public enum EnemyType {Melee, Ranged}
-    [SerializeField]
-    private EnemyType type;
-    [SerializeField]
-    private NavMeshAgent agent;
-    [SerializeField]
-    private Transform playerTarget;
-    [SerializeField]
-    private float attackingRange = 1;
-    [Header("Ranged Enemy:")]
-    [SerializeField]
-    private float retreatRange = 4;
-    [SerializeField]
-    private GameObject projectile;
-    [SerializeField]
-    private Transform firePoint;
-    private float attackRate = 2;
-    float timeAttack;
-    [SerializeField]
-    private Transform backWard;
+namespace Assets {
+    public class Enemy : MonoBehaviour {
+        [SerializeField]
+        private int health = 100;
+        public enum EnemyType { Melee, Ranged }
+        [SerializeField]
+        private EnemyType type;
+        [SerializeField]
+        private NavMeshAgent agent;
+        [SerializeField]
+        private Transform playerTarget;
+        [SerializeField]
+        private float attackingRange = 1;
+        [Header("Ranged Enemy:")]
+        [SerializeField]
+        private float retreatRange = 4;
+        [SerializeField]
+        private GameObject projectile;
+        [SerializeField]
+        private Transform firePoint;
+        private float attackRate = 2;
+        float timeAttack;
+        [SerializeField]
+        private Transform backWard;
 
-    [SerializeField]  private LayerMask attackMask;
-    [SerializeField]  private Transform meleePoint;
-    [SerializeField] private int meleeDamage = 50;
+        [SerializeField] private LayerMask attackMask;
+        [SerializeField] private Transform meleePoint;
+        [SerializeField] private int meleeDamage = 50;
 
-    void Start()
-    {
-        agent.stoppingDistance = attackingRange;
-        playerTarget = GameObject.FindGameObjectWithTag("Player").transform;
-        agent.updateRotation = false;
-        agent.updateUpAxis = false;
-        agent.SetDestination(playerTarget.position);
-
-    }
-
-    void Update()
-    {
-
-
-        switch (type)
-        {
-            case EnemyType.Melee:
-                Melee();
-                break;
-            case EnemyType.Ranged:
-                Ranged();
-                break;
-
-
-
+        void Start() {
+            agent.stoppingDistance = attackingRange;
+            playerTarget = GameObject.FindGameObjectWithTag("Player").transform;
+            agent.updateRotation = false;
+            agent.updateUpAxis = false;
+            agent.SetDestination(playerTarget.position);
 
         }
-    }
+
+        void Update() {
 
 
-    void Melee()
-    {
-        agent.SetDestination(playerTarget.position);
+            switch (type) {
+                case EnemyType.Melee:
+                    Melee();
+                    break;
+                case EnemyType.Ranged:
+                    Ranged();
+                    break;
 
-        Vector3 rotation = playerTarget.position - transform.position;
 
-        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
 
-        firePoint.rotation = Quaternion.Slerp(firePoint.rotation, Quaternion.Euler(0, 0, rotZ), 0.001f);
 
-        if (agent.remainingDistance <= attackingRange)
-        {
-           
-            timeAttack += Time.deltaTime;
+            }
+        }
 
-            if (timeAttack >= attackRate)
-            {
-                RaycastHit2D result = Physics2D.BoxCast(meleePoint.position, new Vector2(2, 1), 0f, meleePoint.forward, 1, attackMask);
-                if(result.collider.GetComponent<PlayerAttack>())
-                {
 
-                    result.collider.GetComponent<PlayerAttack>().TakeDamage(meleeDamage);
+        void Melee() {
+            agent.SetDestination(playerTarget.position);
 
+            Vector3 rotation = playerTarget.position - transform.position;
+
+            float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+
+            firePoint.rotation = Quaternion.Slerp(firePoint.rotation, Quaternion.Euler(0, 0, rotZ), 0.001f);
+
+            if (agent.remainingDistance <= attackingRange) {
+
+                timeAttack += Time.deltaTime;
+
+                if (timeAttack >= attackRate) {
+                    RaycastHit2D result = Physics2D.BoxCast(meleePoint.position, new Vector2(2, 1), 0f, meleePoint.forward, 1, attackMask);
+                    if (result.collider.GetComponent<PlayerAttack>()) {
+
+                        result.collider.GetComponent<PlayerAttack>().TakeDamage(meleeDamage);
+
+                    }
+
+                    //print(result.collider.GetComponent<PlayerAttack>());
+                    timeAttack = 0;
                 }
 
-                //print(result.collider.GetComponent<PlayerAttack>());
-                timeAttack = 0;
+
             }
 
 
-        }
-
-
-
-    }
-    void Ranged()
-    {
-
-        if (Vector2.Distance(transform.position, playerTarget.position) <= retreatRange)
-        {
-            agent.SetDestination(backWard.position);
-            RangedAttack();
-
 
         }
-        else if (agent.remainingDistance <= attackingRange)
-        {
-            agent.SetDestination(playerTarget.position);
-            RangedAttack();
+        void Ranged() {
+
+            if (Vector2.Distance(transform.position, playerTarget.position) <= retreatRange) {
+                agent.SetDestination(backWard.position);
+                RangedAttack();
+
+
+            }
+            else if (agent.remainingDistance <= attackingRange) {
+                agent.SetDestination(playerTarget.position);
+                RangedAttack();
 
 
 
 
 
 
-        }
-        else
-        {
-         
+            }
+            else {
 
-            agent.SetDestination(playerTarget.position);
 
-        }
+                agent.SetDestination(playerTarget.position);
 
+            }
 
 
 
-    }
-
-    void RangedAttack()
-    {
-
-        Vector3 rotation = playerTarget.position - transform.position;
-
-        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
-
-        firePoint.rotation = Quaternion.Euler(0, 0, rotZ);
-        timeAttack += Time.deltaTime;
-
-        if (timeAttack >= attackRate)
-        {
-
-            GameObject thisInstance = Instantiate(projectile, firePoint.position, firePoint.rotation);
-            thisInstance.GetComponent<Ball>().isEnemy = true;
-            timeAttack = 0;
-        }
-
-    }
-
-    public void TakeDamage(int damage)
-    {
-
-        health -= damage;
-
-        if(health <= 0)
-        {
-
-            Destroy(gameObject);
 
         }
 
+        void RangedAttack() {
+
+            Vector3 rotation = playerTarget.position - transform.position;
+
+            float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+
+            firePoint.rotation = Quaternion.Euler(0, 0, rotZ);
+            timeAttack += Time.deltaTime;
+
+            if (timeAttack >= attackRate) {
+
+                GameObject thisInstance = Instantiate(projectile, firePoint.position, firePoint.rotation);
+                thisInstance.GetComponent<Ball>().isEnemy = true;
+                timeAttack = 0;
+            }
+
+        }
+
+        public void TakeDamage(int damage) {
+
+            health -= damage;
+
+            if (health <= 0) {
+
+                Destroy(gameObject);
+
+            }
+
+        }
     }
 }
--- GameJam15/Assets/Scripts/PlayerAttack.cs
+++ GameJam15/Assets/Scripts/PlayerAttack.cs
@@ -1,97 +1,105 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Assets.Scripts.Alchemy;
+namespace Assets {
+    public class PlayerAttack : MonoBehaviour {
+        private int health = 100;
 
-public class PlayerAttack : MonoBehaviour {
-    private int health = 100;
+        public GameObject ball;
+        public Transform firePoint;
+        private Vector2 movement;
+        public Rigidbody2D rb;
+        private float InputX;
+        private float InputY;
+        [SerializeField]
+        private float speed = 8;
+        private Camera mainCam;
+        private Vector3 mousePos;
+        [SerializeField]
+        private Transform pivot;
+        [SerializeField]
+        private float loadingTime = 2f;
+        float timeToLoad;
+        bool isLoading = false;
+        bool isLoaded = false;
+        public bool DisableMovement {
+            get => AlchemyMenu.ShowAlchemyMenu;
+            set { }
+        }
+        void Start() {
+            mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        }
 
-    public GameObject ball;
-    public Transform firePoint;
-    private Vector2 movement;
-    public Rigidbody2D rb;
-    private float InputX;
-    private float InputY;
-    [SerializeField]
-    private float speed = 8;
-    private Camera mainCam;
-    private Vector3 mousePos;
-    [SerializeField]
-    private Transform pivot;
-    [SerializeField]
-    private float loadingTime = 2f;
-    float timeToLoad;
-    bool isLoading = false;
-    bool isLoaded = false;
+        void Update() {
+            LookAtMouse();
+            //Add double tap
+            if (Input.GetKeyDown(KeyCode.Mouse0) && timeToLoad <= 0 && isLoaded) {
+                Instantiate(ball, firePoint.position, firePoint.rotation);
+                timeToLoad = loadingTime;
+                isLoaded = false;
 
-    void Start() {
-        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-    }
 
-    void Update() {
-        LookAtMouse();
-        //Add double tap
-        if (Input.GetKeyDown(KeyCode.Mouse0) && timeToLoad <= 0 && isLoaded) {
-            Instantiate(ball, firePoint.position, firePoint.rotation);
-            timeToLoad = loadingTime;
-            isLoaded = false;
+            }
+            if (Input.GetKeyDown(KeyCode.R) && !isLoading && !isLoaded) {
+                timeToLoad = loadingTime;
+                isLoading = true;
+
+
+            }
+
+            if (timeToLoad > 0 && isLoading) {
+
+                timeToLoad -= Time.deltaTime;
+
+            }
+            else if (timeToLoad <= 0) {
+                isLoaded = true;
+                isLoading = false;
+            }
+        }
+
+        private void FixedUpdate() {
+            if (true) {
+
+            }
+            InputX = Input.GetAxisRaw("Horizontal");
+            InputY = Input.GetAxisRaw("Vertical");
+            movement = new Vector2(InputX, InputY);
+
+            rb.velocity = movement.normalized * speed;
+
+
+
+
 
 
         }
-        if (Input.GetKeyDown(KeyCode.R) && !isLoading && !isLoaded) {
-            timeToLoad = loadingTime;
-            isLoading = true;
+        public void TakeDamage(int damage) {
 
+            health -= damage;
 
-        }
+            if (health <= 0) {
 
-        if (timeToLoad > 0 && isLoading) {
+                //Destroy(gameObject);
 
-            timeToLoad -= Time.deltaTime;
-
-        }
-        else if (timeToLoad <= 0) {
-            isLoaded = true;
-            isLoading = false;
-        }
-    }
-
-    private void FixedUpdate() {
-
-        InputX = Input.GetAxisRaw("Horizontal");
-        InputY = Input.GetAxisRaw("Vertical");
-        movement = new Vector2(InputX, InputY);
-
-        rb.velocity = movement.normalized * speed;
-
-
-
-
-
-
-    }
-    public void TakeDamage(int damage) {
-
-        health -= damage;
-
-        if (health <= 0) {
-
-            Destroy(gameObject);
+            }
 
         }
 
+
+
+        void LookAtMouse() {
+
+            mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+
+            Vector3 rotation = mousePos - pivot.position;
+
+            float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+
+            pivot.rotation = Quaternion.Euler(0, 0, rotZ);
+
+        }
     }
 
-
-
-    void LookAtMouse() {
-
-        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
-
-        Vector3 rotation = mousePos - pivot.position;
-
-        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
-
-        pivot.rotation = Quaternion.Euler(0, 0, rotZ);
-
-    }
 }
